e374b5f01ac702a005948d0676afabd7d7d74392
Jai <jai@MacBook-Pro.broadband>
2020-05-11 18:13:39 +0100

    New Movies Added

--- src/frontend/gallery-get.js
+++ src/frontend/gallery-get.js
@@ -4,6 +4,9 @@ import arrietty from './images/arrietty.jpeg';
 import howls from './images/howls.jpeg';
 import spiritedaway from './images/spiritedaway.jpeg';
 import totoro from './images/totoro1.jpg';
+import evangelion from './images/evangelion.jpg';
+import cowboybebop from './images/cowboybebop.jpg';
+
 
 export default function getGallery() {
     return [
@@ -12,6 +15,8 @@ export default function getGallery() {
       { id: 'arrietty', name: 'Arrietty', videoId: "KzBBIBSi2Vo", cover: arrietty, details: 'Arrietty, a tiny teenager, lives with her parents in the recesses of a suburban home, unbeknown to the homeowner and housekeeper. Like others of her kind, Arrietty remains hidden from her human hosts, but occasionally ventures forth from beneath the floorboards to borrow sugar cubes and other supplies. A secret friendship forms when 12-year-old Shawn meets Arrietty, but their relationship could spell danger for Arrietty\'s family.'},
       { id: 'howlsmovingcastle', name: 'Howls Moving Castle', videoId: "iwROgK94zcM", cover: howls, details: 'Sophie (Emily Mortimer) has an uneventful life at her late father\'s hat shop, but all that changes when she befriends wizard Howl (Christian Bale), who lives in a magical flying castle. However, the evil Witch of Waste (Lauren Bacall) takes issue with their budding relationship and casts a spell on young Sophie, which ages her prematurely. Now Howl must use all his magical talents to battle the jealous hag and return Sophie to her former youth and beauty.'},
       { id: 'spiritedaway', name: 'Spirited Away', videoId: "ByXuk9QqQkk", cover: spiritedaway, details: 'In this animated feature by noted Japanese director Hayao Miyazaki, 10-year-old Chihiro (Rumi Hiiragi) and her parents (Takashi Naitô, Yasuko Sawaguchi) stumble upon a seemingly abandoned amusement park. After her mother and father are turned into giant pigs, Chihiro meets the mysterious Haku (Miyu Irino), who explains that the park is a resort for supernatural beings who need a break from their time spent in the earthly realm, and that she must work there to free herself and her parents.'},
-      { id: 'myneighbourtotoro', name: 'My Neighbour Totoro', videoId: "pU8BRWhj9_o", cover: totoro, details: 'This acclaimed animated tale by director Hayao Miyazaki follows schoolgirl Satsuke and her younger sister, Mei, as they settle into an old country house with their father and wait for their mother to recover from an illness in an area hospital. As the sisters explore their new home, they encounter and befriend playful spirits in their house and the nearby forest, most notably the massive cuddly creature known as Totoro.'}
+      { id: 'myneighbourtotoro', name: 'My Neighbour Totoro', videoId: "pU8BRWhj9_o", cover: totoro, details: 'This acclaimed animated tale by director Hayao Miyazaki follows schoolgirl Satsuke and her younger sister, Mei, as they settle into an old country house with their father and wait for their mother to recover from an illness in an area hospital. As the sisters explore their new home, they encounter and befriend playful spirits in their house and the nearby forest, most notably the massive cuddly creature known as Totoro.'},
+      { id: 'cowboybebop', name: 'Cowboy Bepop', videoId: "Au1AO1xaEts", cover: cowboybebop, },
+      { id: 'evangelion', name: 'Evangelion', videoId: "Au1AO1xaEts", cover: evangelion, },
     ];
   } 
